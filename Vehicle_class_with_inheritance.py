df984a49b999aa1004c8d043bb30724630b42c36
hamza10vr <ha.mza@hotmail.com>
2022-02-12 11:30:29 -0600

    added motorcycle class as a child of Vehicle

--- Vehicle_class_with_inheritance.py
+++ Vehicle_class_with_inheritance.py
@@ -14,13 +14,37 @@ class Vehicle():
 class Car(Vehicle):
     def __init__(self,enginetype):
         super().__init__("CAR")
-        self.wheel =4
-        self.door = 4
+        self.wheels =4
+        self.doors = 4
         self.engine = enginetype
 
     def Driving(self, speed):
         super().Driving(speed)
-        print("My ", self.mode, " ", self.engine , " ", self.bodystyle, "is traveling at ", self.speed)
+        print("My ", self.mode, " ", self.engine , " ", self.bodystyle, "is traveling at ", self.speed," it has ", self.wheels, " and ", self.doors, " doors!")
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, enginetype, hassidecar):
+        super().__init__("Motorcycle")
+        if hassidecar:
+            self.wheels =3
+        else:
+            self.wheels = 2
+
+        self.doors = 0 
+        self.engine = enginetype
+    
+    def Driving(self, speed):
+        super().Driving(speed)
+        print("My ", self.mode, " ", self.engine , " ", self.bodystyle, "is traveling at ", self.speed," it has ", self.wheels, " and ", self.doors, " doors!")
+
+
 
 car1 = Car("gas")
 car1.Driving(20)
+
+mtc = Motorcycle("electric", True)
+mtc.Driving(45)
+
+mtc = Motorcycle("electric", False)
+mtc.Driving(45)
