10f842710a56c77577788055e8854488c6053ef9
Hamza <humza10vr@gmail.com>
2022-05-14 20:06:30 -0500

    added random number generator to choose a number

--- guessing_game_with_real_feel.py
+++ guessing_game_with_real_feel.py
@@ -1,8 +1,15 @@
+
+import random
+
 user_guess = int (input("Guess a number: "))
-number = 5
+number = random.randint(1,100)
+print(number)
 
 while user_guess != number:
-    user_guess = int (input("please guess again: "))
+    if user_guess > number:
+        user_guess = int (input("please guess a lower number: "))
+    else:
+        user_guess = int (input("please guess a higher number: "))
 
 print(f"you have guessed correctly, number was {user_guess} ")
 
