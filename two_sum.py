f1118af8ad989aa55a9732ae2ba5872975d8cd9d
Hamza <humza10vr@gmail.com>
2022-05-17 22:58:50 -0500

    added empty list if target not found

--- two_sum.py
+++ two_sum.py
@@ -9,13 +9,19 @@ target1 = 8
 arr2 = [4,7,2, 6]
 target2 = 12
 
-def two_sum( nums:list[int], target:int) -> list[int]:
+def two_sum( nums, target) : 
     for i in range (len (nums)):
+
         for j in range (i +1, len(nums)):
             sum = nums[i] + nums[j]
             if sum == target:
                 return [i, j]
+    return []
 
 # answer=[]
 answer = two_sum(arr1, target1)
 print(answer)
+
+answer = two_sum(arr2, target2)
+print(answer)
+
