8ae6ff05304e92df05e332db9520ed860061becf
Hamza Tanveer <ha.mza@hotmail.com>
2022-08-29 19:42:40 -0500

    Hangman - word_list, user_input as guess, check if gess is in chosen_word

---
+++ 2022-08-29_hangman.py
@@ -0,0 +1,15 @@
+# hangman game
+# steps:
+import random
+
+word_list = ["ardvark", "baboon", "camel"]
+
+# Randomly choose a word fromt eh word_list and assign it to a variable called chosed_word.
+chosen_word = random.choice(word_list)
+print(chosen_word)
+# ask the user to guess a letter and assign their answer to a variable called gess. Make guess lowercase
+guess = input("Guess a letter: ").lower()
+
+# check if the letter the user guessed (guess) is one of the leters in the chosen_word)
+if guess in chosen_word:
+    print("YES!")
