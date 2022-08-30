092e5e44c3a51b6067f0155683da5e1ece57d5c8
Hamza Tanveer <ha.mza@hotmail.com>
2022-08-29 22:46:32 -0500

    new list with black underscores, and fill in if correct guess

--- 2022-08-29_hangman.py
+++ 2022-08-29_hangman.py
@@ -7,12 +7,22 @@ word_list = ["ardvark", "baboon", "camel"]
 # Randomly choose a word fromt eh word_list and assign it to a variable called chosed_word.
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
+#create blank string equivalent to the lenght of the chosen_word
+display = []
+for _ in range(len(chosen_word)):
+    display.append("_")
+print(display)
+
 # ask the user to guess a letter and assign their answer to a variable called gess. Make guess lowercase
 guess = input("Guess a letter: ").lower()
 
 # check if the letter the user guessed (guess) is one of the leters in the chosen_word)
-for letter in chosen_word:
-    if guess in chosen_word:
+for index, letter in enumerate(chosen_word):
+    if letter == guess:
         print("Right!")
+        display[index] = guess       
     else:
-        print("wrong")
+        print("Wrong")
+
+print(display)
