ec10fd650d27530bc84d6a2a09931bcc01d0fffd
Hamza Tanveer <ha.mza@hotmail.com>
2022-08-31 19:15:49 -0500

    code simplified bye using a specific range(2, number)

--- 2022-08-31_prime_number_checker.py
+++ 2022-08-31_prime_number_checker.py
@@ -1,26 +1,15 @@
 #check if the given input is a prime number or not
 
 def prime_checker(number):
-    prime = True
-    for num in range(1,number+1):
-        if num ==1 or num ==number:
-            continue
-        else:
-            if number % num ==0:
-                prime = False
+    is_prime = True
+    for i in range(2,number):
+        if number % i ==0:
+            is_prime = False
     
-    if prime == True:
+    if is_prime == True:
         print("It's a prime numbner")
     else:
         print("It's not a prime number")
-
-
-
-    # if number % number ==0 and number %1 ==0:
-    #     print("It's a prime numbner")
-    # else:
-    #     print("It's not a prime number")
-
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
