5c8a52ad9453361e2208fe0ab6015055f65e5038
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-02 11:25:03 -0500

    for loop dictionary to calculate max bid , print out winner name and bid

--- 2022-09-02_blind_auction.py
+++ 2022-09-02_blind_auction.py
@@ -20,12 +20,10 @@ print("Welcome to the secret auction program")
 stop_bidding = False
 bidding_enteries = {}
 
-
 while not stop_bidding:
-    name = input("What is your name ?:" )
+    name = input("What is your name ?: " )
     bid = int(input("What is your bid?: "))
     bidding_enteries[name] = bid
-
 
     more_user = (input("Are there any other bidders? Type 'yes' or 'no'. ")).lower()
     if more_user == 'no':
@@ -34,6 +32,17 @@ while not stop_bidding:
     sleep(1)
     os.system('cls')
 
-
 print(bidding_enteries)
-# print(f"The winner is {} with a bid of ${}.")
+
+max_bid = 0
+winner_name = ''
+
+for key in bidding_enteries:
+    if bidding_enteries[key] > max_bid:
+        max_bid = bidding_enteries[key]
+        winner_name = key
+print()
+
+sleep(1)
+os.system('cls')
+print(f"The winner is {winner_name} with a bid of ${bidding_enteries[winner_name]}.")
