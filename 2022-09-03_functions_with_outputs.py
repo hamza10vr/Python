f5b9b910e31baba8fec2d7896cb6f2667d094d33
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-03 17:57:36 -0500

    multiple return statements in a function

--- 2022-09-03_functions_with_outputs.py
+++ 2022-09-03_functions_with_outputs.py
@@ -1,11 +1,17 @@
 #Functions with outputs
 #capatalize the first letter of each word
 
-def format_name(f_name, l_name):
+def format_name(f_name ="", l_name = ""):
+    if f_name =="" or l_name == "":
+        return "You didn't provide valid inputs."
+        
     first_name = f_name.title()
     last_name = l_name.title()
     full_name = first_name + " " + last_name
     return full_name
 
 name = format_name("hamZa", "tANveeR")
+print(name)
+
+name = format_name( "tANveeR")
 print(name)
