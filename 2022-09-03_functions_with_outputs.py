da0827bdf1fe6723337aed2028c91ea6f5434b99
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-04 13:51:52 -0500

    added a docstring for documentation

--- 2022-09-03_functions_with_outputs.py
+++ 2022-09-03_functions_with_outputs.py
@@ -1,7 +1,10 @@
 #Functions with outputs
 #capatalize the first letter of each word
+# adding docstring for documentation
 
 def format_name(f_name ="", l_name = ""):
+    """Take a first and last name and format it to return the title case version of the name."""
+
     if f_name =="" or l_name == "":
         return "You didn't provide valid inputs."
         
@@ -15,3 +18,5 @@ print(name)
 
 name = format_name( "tANveeR")
 print(name)
+
+format_name()
