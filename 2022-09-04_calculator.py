30557ae392f72578e8ea0e5f21a990b28c07f955
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-04 19:32:45 -0500

    difference between return and print in function, return allow for more coding to be done

--- 2022-09-04_calculator.py
+++ 2022-09-04_calculator.py
@@ -32,9 +32,14 @@ for symbol in operations:
     print(symbol)
 
 operation_symbol = input("Pick an operation from the line above: ")
-
 calculation_function = operations[operation_symbol]
+first_answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
-answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(first_answer, num3)
+print(f"{first_answer} {operation_symbol} {num2} = {second_answer}")
