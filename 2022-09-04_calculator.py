66d169f630d43e4198358d74ddb6e05cabe6ce66
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-04 20:18:35 -0500

    added calculator() as a recursive to allow user to start calculation from beginning

--- 2022-09-04_calculator.py
+++ 2022-09-04_calculator.py
@@ -26,22 +26,25 @@ operations = {
     '/': divide
     }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
+def calculator():
+    num1 = int(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
 
-continue_calculation = True
-while continue_calculation:
-    
-    operation_symbol = input("Pick an operation: ")
-    num2 = int(input("what's the next number?: "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    yes_or_no = input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit.: ")
-    if yes_or_no == 'y':
-        num1 = answer
-    else:
-        continue_calculation = False
+    continue_calculation = True
+    while continue_calculation:
+        
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("what's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        yes_or_no = input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit.: ")
+        if yes_or_no == 'y':
+            num1 = answer
+        else:
+            continue_calculation = False
+            calculator()
 
+calculator()
 
