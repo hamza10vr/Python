647ad3ef8e117bfc7ff9c3bff6c3fb5c9c3f4a73
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-17 19:24:32 -0500

    is game over variable and more to calculatore func

--- 2022-09-05_blackjack_capstone_project.py
+++ 2022-09-05_blackjack_capstone_project.py
@@ -13,16 +13,43 @@ print(logo)
 def deal_card():
       """ returns the random card from the deck """
       cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-      return random.choice(cards)
-def calculate_score(list_of_cards):
-      if sum(list_of_cards) == 21:
-            return 0
-      elif sum(list_of_cards) > 21 and 11 in list_of_cards:
-            list_of_cards.remove(11)
-            list_of_cards.append(1)
-            return sum(list_of_cards)
-      else:
-            return sum(list_of_cards)
+      card = random.choice(cards)
+      return card
+
+def calculate_score(cards):
+      """Take a list of cards adn return teh score calculated from them"""
+      if sum(cards) == 21 and len(cards) == 2:
+            return 0 
+      
+      if 11 in cards and sum(cards) > 21:
+            cards.remove(11)
+            cards.append(1)
+      return sum(cards)
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+      user_cards.append(deal_card())
+      computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"    Your cards: {user_cards}, current score: {user_score}")
+print(f"    Computer's first card: {computer_cards[0]} ")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+      is_game_over = True
+# def calculate_score(list_of_cards):
+#       if sum(list_of_cards) == 21:
+#             return 0
+#       elif sum(list_of_cards) > 21 and 11 in list_of_cards:
+#             list_of_cards.remove(11)
+#             list_of_cards.append(1)
+#             return sum(list_of_cards)
+#       else:
+#             return sum(list_of_cards)
 
 def compare(user, computer):
       user_score = calculate_score(user)
