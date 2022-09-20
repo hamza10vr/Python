e88cb4e08e48b2415e70c9ef95f7d7c9a2ff046b
Hamza Tanveer <ha.mza@hotmail.com>
2022-09-19 19:08:20 -0500

    break down the problem and create TODO list

---
+++ 2022-09-19_higher_lower_game_project/2022-09-19_higher_lower_game.py
@@ -0,0 +1,21 @@
+# Break down the Problem
+# games ends when you guess wrong and prints your score
+# randomly picks 2 celebrities compare A against B
+# clears screen at each  answer and updates the score
+# winner becomes A and pops a new B
+# prints final answer when you are wrong
+# Sorry, that's wrong. Final score: 3
+
+
+# TODO: list
+#logo
+#compare A: name, a description, from country
+#logo vs
+#Against B: name, a description, from country
+#who has more followers? Type 'A' or 'B': 
+
+#clears screen. 
+#You're right! Current score: 1
+# print everything again
+
+
