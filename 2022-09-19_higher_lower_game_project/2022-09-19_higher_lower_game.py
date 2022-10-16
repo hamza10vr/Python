92403efcf137217a465a1ea48278f50831518f16
Hamza Tanveer <ha.mza@hotmail.com>
2022-10-16 16:03:37 -0500

    add check_answer, score increment functionality

--- 2022-09-19_higher_lower_game_project/2022-09-19_higher_lower_game.py
+++ 2022-09-19_higher_lower_game_project/2022-09-19_higher_lower_game.py
@@ -1,17 +1,26 @@
 
-from art import logo
+from art import logo, vs
 from game_data import data
 import random
 
 def format_data(account):
-    """format the account data into printable format."""
+    """Takes the account data returns the printable format."""
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return (f"{account_name}, a {account_descr}, from {account_country}")
 
+# check if user is correct. a
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and follower counts and returns if they got it right"""
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess =='b'
+
 # Display art
 print(logo)
+score = 0
 
 # Generate a random account from the game data
 account_a = random.choice(data)
@@ -20,17 +29,25 @@ if account_a == account_b:
     account_b = random.choice(data)
 
 print(f"Compare A: {format_data(account_a)}.")
-print(f"Compare B: {format_data(account_b)}.")
+print(vs)
+print(f"Against B: {format_data(account_b)}.")
 
 # Ask user for a guess
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# check if user is correct. 
 ## get follower count of each account.
-## use if statement to check if user is correct.
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
 # give user feedback on their guess.
-
 # score keeping.
+
+if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}")
+else:
+        print(f"Sorry , that's wrong . Final score: {score}")
 
 # Make the game repeatable.
 
