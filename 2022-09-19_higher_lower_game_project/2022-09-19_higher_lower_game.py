70ad122b4dc8ab8ceac97950e616eb33410a6995
Hamza Tanveer <ha.mza@hotmail.com>
2022-10-16 16:06:38 -0500

    add loop to repeat the code until user gets it wrong

--- 2022-09-19_higher_lower_game_project/2022-09-19_higher_lower_game.py
+++ 2022-09-19_higher_lower_game_project/2022-09-19_higher_lower_game.py
@@ -21,33 +21,38 @@ def check_answer(guess, a_followers, b_followers):
 # Display art
 print(logo)
 score = 0
+game_should_continue = True
 
-# Generate a random account from the game data
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:
+
+while game_should_continue == True:
+
+    # Generate a random account from the game data
+    account_a = random.choice(data)
     account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print(vs)
-print(f"Against B: {format_data(account_b)}.")
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
 
-# Ask user for a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    # Ask user for a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-## get follower count of each account.
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    ## get follower count of each account.
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-# give user feedback on their guess.
-# score keeping.
+    # give user feedback on their guess.
+    # score keeping.
 
-if is_correct:
-    score += 1
-    print(f"You're right! Current score: {score}")
-else:
-        print(f"Sorry , that's wrong . Final score: {score}")
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+            print(f"Sorry , that's wrong . Final score: {score}")
+            game_should_continue = False
 
 # Make the game repeatable.
 
