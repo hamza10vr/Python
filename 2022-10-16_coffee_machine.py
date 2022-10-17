20600e9e2f4ede3c7e5f46f463b8bc5804248f21
Hamza Tanveer <ha.mza@hotmail.com>
2022-10-16 19:48:55 -0500

    check resources fucntion

--- 2022-10-16_coffee_machine.py
+++ 2022-10-16_coffee_machine.py
@@ -29,19 +29,40 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+profit = 0
 
-
-def report(resources):
+def generate_report(resources, money):
     """prints report of remaining resources"""
     return f"Water: {resources['water']}\n" \
            f"Milk: {resources['milk']}\n" \
-           f"Milk: {resources['coffee']}\n"
+           f"Coffee: {resources['coffee']}\n" \
+           f"Money: ${profit}\n"
+
+is_on = True
+
+def check_resources(order_ingredients):
+    """Takes coffe input checks it against to resources to verify if enough ingredients"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is Not enough {item}")
+            return False
+        return True
+
+while is_on:
+    # Initial prompt What would you like? (expresso/latte/cappuccino):
+    choice = input("What would you like? (espresso/latte/cappuccino):  ").lower()
+    if choice == 'report':
+        print(generate_report(resources, profit))
+    elif choice == 'off':
+        is_on = False
+    else:
+        drink = MENU[choice]
+        check_resources(drink['ingredients'])
+        # choice == 'espresso':
+        # check_resources(resources,choice)
+        # print("move ahead")
 
 
-# Initial prompt What would you like? (expresso/latte/cappuccino):
-coffee_type = input("What would you like? (espresso/latte/cappuccino):  ").lower()
-if coffee_type == 'report':
-    print(report(resources))
 
 # TODO: report - prints resources and money inserted
 
