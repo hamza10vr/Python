4ee84bbace5733b94509bc6e97feaf97e320d5b9
hamza10vr <48808837+hamza10vr@users.noreply.github.com>
2022-11-21 04:29:25 +0000

    outline for the game

---
+++ 2022-11-17_game_save_zortan.py
@@ -0,0 +1,14 @@
+"""
+Zortan is under attack!!! Thanos has arrived!
+---------------------------------------------
+Since Zortan is under attack, Louis calls his Avenger friends from earth.
+Avengers receive his call and sends 4 avengers to save Zortan.
+1. Ironman
+4. Black Widow
+2. Spiderman
+3. Hulk
+Each avenger has its attacking power and they have to fight Thanos
+to save Zortan.
+Avengers can attack only in pairs and get only 3 chances to kill Thanos,
+or else Thanos will kill avengers and we loose the game.
+"""
