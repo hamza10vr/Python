99a257b544e2a16b20c205ce639f0566089ec77f
hamza10vr <48808837+hamza10vr@users.noreply.github.com>
2022-11-22 22:18:02 +0000

    add superhero and supervillians dictionary and logic for creating attack

--- 2022-11-22_game_save_zortan_2.py
+++ 2022-11-22_game_save_zortan_2.py
@@ -13,17 +13,41 @@ this time each character get its own structure and defined parameters, so as you
 can see our code getting better and certainly we are going to make is awesome
 as we progress with future modules.
 """
-
+#A special typing construct to indicate to type checkers that a name cannot be re-assigned or overridden in a subclass.
 from typing import Final
+from random import randint
 
-IRONMAN_ATTACK_POWER: Final[int] = 250
-BLACKWIDOW_ATTACK_POWER: Final[int] = 180
-SPIDERMAN_ATTACK_POWER: Final[int] = 190
-HULK_ATTACK_POWER: Final[int] = 300
+#Typing alias
+Character = dict[str, str | int]
 
-thanos_life: int = 1500
+#Superheros
+IRONMAN: Final[Character] = {"name": "Ironman", "attack_power": 250, "life": 1000}
+BLACKWIDOW: Final[Character] = {"name": "BlackWidow", "attack_power": 180, "life": 800}
+SPIDERMAN: Final[Character] = {"name": "Spiderman", "attack_power": 190, "life": 700}
+HULK: Final[Character] = {"name": "Hulk", "attack_power": 300, "life": 1100}
+
+#Super Villains
+THANOS: Final[Character] = {"name": "thanos", "attack_power": 1500, "life": 1500 }
+REDSKULL: Final[Character] = {"name": "Redskull", "attack_power": 300, "life": 800 }
+PROXIMA: Final[Character] = {"name": "Proxima", "attack_power": 320, "life": 800 }
+
+# All Super Heros & Super Villains
+superheros: list[Character] = [IRONMAN, BLACKWIDOW, SPIDERMAN, HULK]
+supervillains: list[Character] = [THANOS, REDSKULL, PROXIMA]
+
+#Helper Variables
 choice: int = 0
 attack_number: int = 0
 WIN_MSG: Final[str] = "\n######  You successfully saved Zortan!!!  #####\n"
 LOST_MSG: Final[str] = "\n#####  Thanos killed Avengers and captured Zortan!!  #####\n"
 
+#logic for creating attack
+for attack in range(3):
+    # each iteration get a new hero & villain
+    hero_index = randint(0,3)
+    villain_index = randint(0,2)
+    # helper variables
+    current_superhero = superheros[hero_index]
+    current_supervillain = supervillains[villain_index]
+
+    print(f"Attack: {attack + 1} => {current_superhero['name']} is going to fight with {current_supervillain['name']} ")
